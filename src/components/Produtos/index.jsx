5054ef5e2ccae297aea5dea5d7e3650189a3e28a
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-07-16 21:51:28 -0300

    produto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -19,6 +19,15 @@ const ContainerStyled = styled.div`
    flex-wrap: wrap;
    justify-content: center;
    gap: 2px;
+
+   @media screen and (max-width: 380px) {
+           display: flex;
+           flex-wrap: wrap;
+        }
+            @media screen and (max-width: 470px) {
+                display: flex;
+                flex-wrap: wrap;
+        }
 `;
 
 const Title = styled.h1` 
@@ -32,8 +41,6 @@ const Title = styled.h1`
 const ListaProduto = styled.div`
   text-align: center;
   margin: 20px;
-
-
 `;
 
 const ImgProdutoStyled = styled.img`
@@ -45,23 +52,53 @@ const ImgProdutoStyled = styled.img`
         scale: 1.2;
         transition: 0.6s;
       }
+
+      @media screen and (max-width: 380px) {
+           width: 100px;
+           height:100px;
+        }
+            @media screen and (max-width: 470px) {
+                width: 100px;
+                height: 100px;
+        }
 `;
 
 const TitleStyled = styled.h1`
      font-size: 20px;
      font-weight: bold;
-     
+  
+     @media screen and (max-width: 380px) {
+           font-size: 9px;
+        }
+            @media screen and (max-width: 470px) {
+                font-size: 9px;
+        }
+  
 `;
 
 const DescricaoStyled = styled.p`
     font-size: 13px;
     font-weight: bold;
     text-wrap: balance;
+
+    @media screen and (max-width: 380px) {
+           font-size: 8px;
+        }
+            @media screen and (max-width: 470px) {
+                font-size: 8px;
+        }
 ` ;
 
 const Valor = styled.p`
      font-size: 25px;
      font-weight: bold;
+
+     @media screen and (max-width: 380px) {
+           font-size: 13px;
+        }
+            @media screen and (max-width: 470px) {
+                font-size: 13px;
+        }
     
 `;
 
@@ -83,7 +120,7 @@ export default function Produtos() {
         <ContainerStyled>
         {dbRecomendados.map((post) => (
         <ListaProduto key={post} >
-            <ImgProdutoStyled src={post.img} alt="foto" width='250' height='250' />
+            <ImgProdutoStyled src={post.img} alt="foto" width='170' height='170' />
             <TitleStyled>{post.nome}</TitleStyled>
             <Valor>{post.valor}</Valor>
             <DescricaoStyled>{post.descricao}</DescricaoStyled>
